4fbbb364fbdeafd94d0186e37c0a8f78f364d7b5
Mauricio <mauriciofloresc6@gmail.com>
2022-05-30 11:35:11 -0500

    Agregamos el ejercicio 3

---
+++ Ciclos/js/main.js
@@ -0,0 +1,7 @@
+// Encontrar los primeros números pares entre el 1 y el 100
+// inicio ;      comparacion        //incremento
+for (let index = 1; index <= 100; index++) {
+    if ((index%2) == 0) {
+        console.log(index + "es un numero par");
+    }
+}// for index
--- js/main.js
+++ js/main.js
@@ -2,21 +2,21 @@
 
 // 2.  Solicitar 3 numeros (entre el 1 y el 100)y definir el menor de 3 numeros
 
-let a=prompt("¿Ingresa el primer numero del 1 al 100?", "" );
-let b=prompt("¿Ingresa el segundo numero del 1 al 100?", "" );
-let c=prompt("¿Ingresa el tercer numero del 1 al 100?", "" );
+// let a=prompt("¿Ingresa el primer numero del 1 al 100?", "" );
+// let b=prompt("¿Ingresa el segundo numero del 1 al 100?", "" );
+// let c=prompt("¿Ingresa el tercer numero del 1 al 100?", "" );
 
-parseFloat=(a );
-parseFloat=(b );
-parseFloat=(c );
+// parseFloat=(a );
+// parseFloat=(b );
+// parseFloat=(c );
 
-if ((a>b) && (a>c)){
-    console.log(a);
-} else if((b>c) && (b>a)){
-    console.log(b);
-}else if ((c>b) && (c>a)){
-    console.log(c);
-}
+// if ((a>b) && (a>c)){
+//     console.log(a);
+// } else if((b>c) && (b>a)){
+//     console.log(b);
+// }else if ((c>b) && (c>a)){
+//     console.log(c);
+// }
 
 //segundo ejercicio
 // let a=prompt("¿Ingresa el primer numero del 1 al 100?", "" );
@@ -78,3 +78,41 @@ if ((a>b) && (a>c)){
 // } else if (c == (a + b)) {
 //     alert("La suma del primero mas el segundo es igual al tercero")
 // }
+
+// ejercio 3. Adivinar entre el 1 al 100 en el menor numero de pasos
+
+
+    // alert("Piensa en un numero del 1 al 100 ");
+    // prompt("A ese numero multipliquelo por 5");
+    // prompt("Despues sumale 12");
+    // prompt("Luego multipliquelo por 10");
+    // prompt("despues sumele 5");
+    // prompt("Multiplique por 2");
+    //     let num=parseFloat(prompt("¿Que numero te dio de resultado"));
+    //     let b= (num - 250);
+    //     let a= (b / 100);
+    //       alert("Tu numero es el :" + a);
+
+    function adivinar() {
+      let superior = 100;
+      let inferior = 0;
+      let noEncontrado = true;
+      while (noEncontrado) {
+        let mid = parseInt(inferior + ((superior-inferior) / 2));
+        console.log(mid);
+        console.log(((superior-inferior)/2));
+        if(((superior-inferior)/2)<1){
+          noEncontrado=false;
+          alert("Tu numero es el " +  (parseInt(mid)+ 1));
+          break;
+        }
+        let res = confirm("Tu numero es menor o igual a " + mid);
+        if(res){
+          superior=mid;
+        }else{
+          inferior=mid
+        }//if
+        console.log(inferior, superior);
+      }//while
+    }//adivinar
+    adivinar();
